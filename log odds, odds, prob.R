05b701a7dbcd83484773111a49cfccb595e213db
Roger Day <notsofeo@gmail.com>
2025-05-06 12:34:03 -0400

    rescaled the prob axis

--- log odds, odds, prob.R
+++ log odds, odds, prob.R
@@ -6,23 +6,26 @@ oddscolor = 'blue'
 probcolor = 'green'
 axisNameFont = c('serif', 'bold italic')
 
-yrange = c(-1,1)
+yrange = c(-5,5)
 plot(c(-1,axis.prob), yrange, axes=F, pch=' ',
      xlab='',ylab='')
 logoddslist = seq(yrange[1],yrange[2],length=100)
 oddslist = exp(logoddslist)
 problist = odds.to.prob(oddslist)
-
+rescaled.prob = function(p,
+                         expander = yrange[2]-yrange[1], 
+                         offset = yrange[1])
+  p*expander + offset
 sapply(logoddslist, function(logodds)
   {
   odds = exp(logodds)
   lines(c(axis.log.odds, axis.odds), c(logodds, odds), col='lightgrey')
   lines(c(axis.odds, axis.prob), 
-        c(odds, odds.to.prob(odds)), , col='lightgrey')
+        c(odds, rescaled.prob(odds.to.prob(odds))), col='lightgrey')
 })
 abline(v=axis.log.odds, col=logoddscolor)
 lines(c(axis.odds, axis.odds), c(0, max(oddslist)), lwd=3, col=oddscolor)
-lines(c(axis.prob, axis.prob), c(0,1), lwd=3, col=probcolor)
+lines(c(axis.prob, axis.prob), rescaled.prob(c(0,1)), lwd=3, col=probcolor)
 
 text(axis.log.odds, yrange[2], 'log odds', col=logoddscolor, xpd=NA, adj = c(0.5,-2.2), vfont=axisNameFont)
 text(axis.odds, yrange[2], 'odds', col=oddscolor, xpd=NA, adj = c(0.5,-2.2), vfont=axisNameFont)
@@ -34,16 +37,16 @@ text(axis.odds, yrange[2], 'infinity', col=oddscolor, xpd=NA, adj = c(0.5,-1))
 text(axis.prob, yrange[2], 'prob', col=probcolor, xpd=NA, 
      vfont = axisNameFont, adj = c(0.5,-2.2))
 # Hershey to see options
-text(axis.prob, 0, 'zero', col=probcolor, xpd=NA, adj=c(-0.2,0.6))
-text(axis.prob, 1, 'one', col=probcolor, xpd=NA, adj=c(-0.2,-0.4))
+text(axis.prob, rescaled.prob(0), 'zero', col=probcolor, xpd=NA, adj=c(-0.2,0))
+text(axis.prob, rescaled.prob(1), 'one', col=probcolor, xpd=NA, adj=c(-0.2,-0))
 
 temparrow = function(x,y,...)
-  arrows(x0=x[1], y0=y[1], x1=x[2], y1=y[2], length=0.025, ...)
+  arrows(x0=x[1], y0=y[1], x1=x[2], y1=y[2], length=0.05, ...)
 for(logodds in logoddslist[ (1:length(logoddslist)) %%5 == 0]){
   odds = exp(logodds)
   temparrow(c(axis.log.odds, axis.odds), c(logodds, odds), col='black')
   temparrow(c(axis.odds, axis.prob), 
-        c(odds, odds.to.prob(odds)), col='black')
+        c(odds, rescaled.prob(odds.to.prob(odds))), col='black')
   readline("wait")
 }
 
