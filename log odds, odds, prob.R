1a5e71e63adf269a410508c3bac9bfd5f12831c4
Roger Day <notsofeo@gmail.com>
2025-05-06 13:35:43 -0400

    rescaled the odds
    selected some probs to show when exceeded.

--- log odds, odds, prob.R
+++ log odds, odds, prob.R
@@ -12,6 +12,10 @@ plot(c(-1,axis.prob), yrange, axes=F, pch=' ',
 logoddslist = seq(yrange[1],yrange[2],length=100)
 oddslist = exp(logoddslist)
 problist = odds.to.prob(oddslist)
+rescaled.odds = function(od,
+                         expander = 0.7*(yrange[2]-yrange[1]), 
+                         offset = yrange[1]+ 0.1*expander)
+  od*expander + offset
 rescaled.prob = function(p,
                          expander = yrange[2]-yrange[1], 
                          offset = yrange[1])
@@ -19,20 +23,21 @@ rescaled.prob = function(p,
 sapply(logoddslist, function(logodds)
   {
   odds = exp(logodds)
-  lines(c(axis.log.odds, axis.odds), c(logodds, odds), col='lightgrey')
+  lines(c(axis.log.odds, axis.odds), 
+        c(logodds, rescaled.odds(odds)), col='lightgrey')
   lines(c(axis.odds, axis.prob), 
-        c(odds, rescaled.prob(odds.to.prob(odds))), col='lightgrey')
+        c(rescaled.odds(odds), rescaled.prob(odds.to.prob(odds))), col='lightgrey')
 })
-abline(v=axis.log.odds, col=logoddscolor)
-lines(c(axis.odds, axis.odds), c(0, max(oddslist)), lwd=3, col=oddscolor)
+abline(v=axis.log.odds, col=logoddscolor, lwd=3)
+lines(c(axis.odds, axis.odds), rescaled.odds(c(0, max(oddslist))), lwd=3, col=oddscolor)
 lines(c(axis.prob, axis.prob), rescaled.prob(c(0,1)), lwd=3, col=probcolor)
 
 text(axis.log.odds, yrange[2], 'log odds', col=logoddscolor, xpd=NA, adj = c(0.5,-2.2), vfont=axisNameFont)
 text(axis.odds, yrange[2], 'odds', col=oddscolor, xpd=NA, adj = c(0.5,-2.2), vfont=axisNameFont)
-text(axis.log.odds, yrange[1], 'neg inf', col=logoddscolor, xpd=NA)
+text(axis.log.odds, yrange[1], 'neg inf', col=logoddscolor, xpd=NA, adj = c(0.5,2))
 text(axis.log.odds, yrange[2], 'infinity', col=logoddscolor, xpd=NA, adj = c(0.5,-1))
-text(axis.odds, 0, 'zero', col=oddscolor, xpd=NA, adj = c(0,1))
-text(axis.odds, yrange[2], 'infinity', col=oddscolor, xpd=NA, adj = c(0.5,-1))
+text(axis.odds, rescaled.odds(0), 'zero', col=oddscolor, xpd=NA, adj = c(0,1))
+text(axis.odds, (yrange[2]), 'infinity', col=oddscolor, xpd=NA, adj = c(0.5,-1))
 
 text(axis.prob, yrange[2], 'prob', col=probcolor, xpd=NA, 
      vfont = axisNameFont, adj = c(0.5,-2.2))
@@ -42,12 +47,23 @@ text(axis.prob, rescaled.prob(1), 'one', col=probcolor, xpd=NA, adj=c(-0.2,-0))
 
 temparrow = function(x,y,...)
   arrows(x0=x[1], y0=y[1], x1=x[2], y1=y[2], length=0.05, ...)
-for(logodds in logoddslist[ (1:length(logoddslist)) %%5 == 0]){
+logoddslist.reduced = 
+   logoddslist[ (1:length(logoddslist)) %%5 == 0]
+probsToShow = c(0.1, 0.25, 0.5, 0.75, 0.9)
+for(logodds in logoddslist.reduced){
   odds = exp(logodds)
-  temparrow(c(axis.log.odds, axis.odds), c(logodds, odds), col='black')
+  thisProb = odds.to.prob(odds)
+  temparrow(c(axis.log.odds, axis.odds), c(logodds, rescaled.odds(odds)), col='black')
   temparrow(c(axis.odds, axis.prob), 
-        c(odds, rescaled.prob(odds.to.prob(odds))), col='black')
+        c(rescaled.odds(odds), rescaled.prob(odds.to.prob(odds))), col='black')
+  biggerThanWhich = which(thisProb>probsToShow)
+  if(length(biggerThanWhich) > 0) {
+    newProb = max(probsToShow[biggerThanWhich])
+text(axis.prob, rescaled.prob(newProb),
+     newProb, col=probcolor, xpd=NA, adj=c(-0.2,0))
+  }
   readline("wait")
 }
+
 
 
