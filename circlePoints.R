9d2debbbfe9595248f86cbdf17002eb1a56a28cb
Roger Day <notsofeo@gmail.com>
2025-05-15 12:13:34 -0400

    nice circlePoints

--- circlePoints.R
+++ circlePoints.R
@@ -1,4 +1,4 @@
-NPOINTS = 20
+NPOINTS = 10
 circlePoints = exp(1i * 2*pi * (1:NPOINTS)/NPOINTS)
 circlePointsXY = cbind(Re(circlePoints), Im(circlePoints))
 round(circlePointsXY, 3)
@@ -6,7 +6,29 @@ plot((-1):1, (-1):1, axes=F, pch=' ', xlab='', ylab='')
 points(circlePoints, pch=' ')
 # for(i in 1:NPOINTS)
 #   lines(circlePointsXY[c(i,ifelse(i==NPOINTS, 1, i+1)),], col='grey', lty=2)
-for(i in 1:NPOINTS) for(j in 1:NPOINTS)
-  lines(circlePointsXY[c(i,ifelse(j==NPOINTS, 1, j+1)),], 
-        col='grey', lty=3)
-text(circlePoints, labels = paste('R',1:NPOINTS), xpd=NA)
+text(circlePoints[1], labels = paste('R',1), xpd=NA)
+for(i in 1:(NPOINTS-1)) {
+  for(j in (i+1):NPOINTS) {
+    cat(i, ' ', j, '\n')
+    nextPoint =  ifelse(j==(NPOINTS+1), 1, j)
+    lines(circlePointsXY[c(i, nextPoint),], 
+          col=i, lty=1)
+    text(circlePoints[nextPoint], 
+         labels = paste('R',nextPoint), xpd=NA)
+    profvis::pause(0.2)
+  }
+}
+test = function() {
+for(i in 1:(NPOINTS-1)) {
+  for(j in (i+1):NPOINTS) {
+    cat(i, ' ', j, '\n')
+    nextPoint =  ifelse(j==(NPOINTS+1), 1, j)
+    lines(circlePointsXY[c(i, nextPoint),], 
+          col='red', lty=3)
+    text(circlePoints[nextPoint], 
+         labels = paste('R',nextPoint), xpd=NA)
+    readline()
+  }
+}
+}
+# text(circlePoints, labels = paste('R',1:NPOINTS), xpd=NA)
